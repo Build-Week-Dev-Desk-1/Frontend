ce9abfa8dab6b73174b5e0a5f77c000b7786cc3c
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-27 15:27:00 -0400

    Initial Push

---
+++ dev-desk/src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+import { axiosWithAuth } from "./utils/axiosWithAuth";
+
+import "./App.css";
+
+export default function App() {
+  // const [credentials, setCredentials] = (e) =>
+  //   useState({
+  //     username: "",
+  //     password: "",
+  //     email: "",
+  //   });
+
+  return <h1>Hi from App.js</h1>;
+}
