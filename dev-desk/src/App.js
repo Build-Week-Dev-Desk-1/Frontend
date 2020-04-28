4c7eb56caa66281b8cdd07079919306ebf28108a
DenisChuvilin <denischuv@gmail.com>
2020-04-28 12:33:36 -0700

    cleaned app component

--- dev-desk/src/App.js
+++ dev-desk/src/App.js
@@ -1,37 +1,16 @@
 import React, { useEffect, useState, createContext } from "react";
-import { useHistory } from "react-router-dom";
-import { axiosWithAuth } from "./utils/axiosWithAuth";
-import * as yup from "yup";
-
-import CreateTicket from "./components/CreateTicket";
-import "./App.css";
 
 //importing components
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Header from "./components/Header";
+import CreateTicket from "./components/CreateTicket";
+import "./App.css";
 
-const initialFormValues = {
-  name: "",
-  password: ""
-};
+
 
 export default function App() {
-  // Keep all initial state here
-  const [formValues, setFormValues] = useState(initialFormValues);
-
-  let history = useHistory();
-
-  const onSubmit = e => {
-    e.preventDefault();
-    axiosWithAuth()
-      .post("/api/auth/login", formValues)
-      .then(res => {
-        console.log(res);
-        localStorage.setItem("token", JSON.stringify(res.data.payload));
-        history.push("/protected");
-      });
-  };
+ 
   return (
     <div>
       <Header />
