1b59565dc1ac10f8b17a42114a531b60df813bd7
Cody Branson <codyboi7000@gmail.com>
2020-04-30 19:31:21 -0600

    removed h4 from sign up page

--- dev-desk/src/components/Signup.js
+++ dev-desk/src/components/Signup.js
@@ -15,13 +15,9 @@ const formSchema = yup.object().shape({
   password: yup
     .string()
     .min(5, "*a password is required")
-<<<<<<< HEAD
     .required("this is req"),
 
   role: yup.string().required("this is required")
-=======
-    .required("this is req")
->>>>>>> 8f2de651545e2c2a0ebc37f283dd7ca0074c8a72
 });
 
 const initialFormValues = {
@@ -139,11 +135,10 @@ export default function Signup() {
             ></input>
           </label>
         </div>
-<<<<<<< HEAD
 
         <div className="choice">
           <label>
-            <h5>Are you a Student or a Tech</h5>
+            <h5></h5>
             <select
               value={formValues.role}
               onChange={onInputChange}
@@ -155,23 +150,6 @@ export default function Signup() {
           </label>
         </div>
 
-=======
-        <div className="admin">
-          <label>
-            <input
-              checked={
-                formValues.role
-                  ? (formValues.role = "tech")
-                  : (formValues.role = "student")
-              }
-              onChange={onCheckboxChange}
-              name="role"
-              type="checkbox"
-            />
-            <span className="splash-h4s"> Helper</span>
-          </label>
-        </div>
->>>>>>> 8f2de651545e2c2a0ebc37f283dd7ca0074c8a72
         <div className="submit-button">
           <label>
             <button className="login-button">Sign Up</button>
